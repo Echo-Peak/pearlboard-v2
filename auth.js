07cc93722adefa7332ecafff695c4a813fd2a129
Mike w <echopeakdev@gmail.com>
2021-06-26 22:33:03 -0400

    Added auth phase 3

--- auth.js
+++ auth.js
@@ -1,6 +1,20 @@
 const { Router } = require('express');
+const passport = require('passport');
+const passportFacebook = require('passport-facebook');
+const passportGoogle = require('passport-google-oauth');
 
 let router = Router();
+
+// passport.use(new passportFacebook({
+    
+// }));
+
+// passport.use(new passportGoogle.OAuth2Strategy({
+    
+// }));
+
+router.use(passport.initialize());
+router.use(passport.session());
 
 router.get('/login/facebook',()=>{
 
--- server.js
+++ server.js
@@ -1,27 +1,38 @@
+process.env.DB_CONNECTED = false;
+
 const express = require('express');
 const next = require('next');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const keys = require("./server/config/keys");
 const stripe = require('stripe')(keys.stripeSecretKey);
 const routes = require('./routes');
 const AuthEndpoints = require('./auth');
+const mongoose = require('mongoose');
+
 
 const dev = process.env.NODE_ENV !== 'production';
 
 const app = next({ dir: '.', dev });
 const handle = routes.getRequestHandler(app);
 
-app.prepare().then(() => {
+function startServer(){  
     const server = express();
         // Static files
         // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
     server.use('/images', express.static(path.join(__dirname, 'images'), {
         maxAge: dev ? '0' : '365d'
     }));
-
+    server.use(session({
+        secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+      }));
     server.use(bodyParser.json());
-    
+
+
     server.get('/healthcheck',(req, res) => res.status(200).end('ok'));
     server.use('/auth', AuthEndpoints)
     server.get('*', (req, res) => {
@@ -42,6 +53,19 @@ app.prepare().then(() => {
 
     server.listen(PORT, (err) => {
         if (err) throw err
-        console.log(`> Read on http://localhost:${PORT}`)
+        console.log(`Lisiening on port ${PORT}`);
     });
+}
+
+app.prepare()
+.then(() => mongoose.connect(process.env.DB_URL))
+.then(()=>{
+    console.log('Services started succesfully!');
+    process.env.DB_CONNECTED = true;
 })
+.catch(err =>{
+    console.log(err);
+})
+.finally(()=>{
+    startServer();
+});
